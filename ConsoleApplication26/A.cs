df8eaac5084cf6ea5c966757cd117a5eb4657921
eszterszoukup <eszoukup@gmail.com>
2017-02-23 12:32:42 +0100

    ABCD

---
+++ ConsoleApplication26/A.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication26
+{
+    public class A
+    {
+        public void S1()
+        {
+            Console.WriteLine("A :: S1");
+            V1();
+        }
+
+        private void S3()
+        {
+            Console.WriteLine("A :: S3");
+            S1();
+        }
+
+        public void S2()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected virtual void V1()
+        {
+            Console.WriteLine("A :: V1");
+            S3();
+        }
+    }
+}
